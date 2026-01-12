e1ad2485ada265603be34f5508c503e30718b9f4
yash <ysiddhapura6@gmail.com>
2026-01-12 09:42:30 +0530

    first commit

---
+++ Back-end/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port , (err)=> {
+    if(err){
+        console.log(err);
+        return false;
+    }
+    console.log(`Server is running on port ${port}`);
+})
+
+module.exports = app;
