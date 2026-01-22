9e578abc1e717918c5c0ec582da0c3c68bdab7d7
yash <ysiddhapura6@gmail.com>
2026-01-22 11:07:11 +0530

    check 5

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -24,6 +24,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use((req, res, next) => {
     if (req.method === 'OPTIONS') {
+        const origin = req.headers.origin;
+        if (origin) {
+            res.header('Access-Control-Allow-Origin', String(origin));
+            res.header('Vary', 'Origin');
+        }
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         return res.sendStatus(204);
     }
     return next();
